d02639b569bfee4238ac0acaa3b2bc8363b8a03b
Luana Leite da Silva Portezan <re047902@qintess.com>
2024-11-19 15:10:21 -0300

    codigo simples de js

---
+++ src/javascript.js
@@ -0,0 +1,10 @@
+//exemplo de uso de variáveis
+
+let nome = "João";
+let idade = 25;
+let peso = 80;
+let altura = 1.80;
+
+let imc = peso / (altura * altura);
+let anoNascimento = 2023 - idade;
+console.log("Olá " + nome + ", você tem " + idade + " anos, nasceu em " + anoNascimento + ", tem " + altura + " de altura, pesa " + peso + "kg seu IMC é " + imc + "kg/m2");
